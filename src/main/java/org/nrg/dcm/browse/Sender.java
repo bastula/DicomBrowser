8ba6951b064ca70fa5901bb745e47ad78331b9ab
Kevin Archie <karchie@maritim.us>
2008-09-18 20:20:49 +0000

    Moved DicomSender to DicomUtils and changed its preferred API

--- src/main/java/org/nrg/dcm/browse/Sender.java
+++ src/main/java/org/nrg/dcm/browse/Sender.java
@@ -12,68 +12,72 @@ import javax.swing.ProgressMonitor;
 
 import org.dcm4che2.data.DicomObject;
 import org.dcm4che2.net.ConfigurationException;
+import org.dcm4che2.net.NetworkApplicationEntity;
+import org.dcm4che2.net.NetworkApplicationEntityBuilder;
+import org.dcm4che2.net.NetworkConnection;
+import org.dcm4che2.net.NetworkConnectionBuilder;
 import org.dcm4che2.net.TransferCapability;
 
 import org.nrg.dcm.DicomSender;
 import org.nrg.dcm.edit.Statement;
 
 final class Sender extends Exporter {
-  private static final String MAKE_CONN_MSG = "Making network connection";
-  private static final String BAD_CONN_MSG = "Unable to make network connection: %1$s";
+	private static final String MAKE_CONN_MSG = "Making network connection";
+//	private static final String BAD_CONN_MSG = "Unable to make network connection: %1$s";
 
-  private final String host;
-  private final String port;
-  private final String aeTitle;
-  private int progress = 0;
-  private final ProgressMonitor pm;
-  private DicomSender sender = null;
-  private final TransferCapability[] tcs;
-  
+	private int progress = 0;
+	private final ProgressMonitor pm;
+	private DicomSender sender;
 
-  Sender(final String host, final String port, final String aeTitle,
-      final TransferCapability[] tcs, final Collection<File> files,
-      final Statement statements, final ProgressMonitor pm) {
-    super(statements, files);
-    this.host = host;
-    this.port = port;
-    this.aeTitle = aeTitle;
-    this.tcs = tcs;
-    this.pm = pm;
-  }
-  
-  @Override
-  void open() throws IOException {
-    if (pm != null)
-      pm.setNote(MAKE_CONN_MSG);
-    
-    // Open a connection to the remote AE.
-    try {
-      sender = new DicomSender("DicomBrowser", tcs, host, port, aeTitle);
-    } catch (ConfigurationException e) {
-      throw new IOException(String.format(BAD_CONN_MSG, e.getMessage()));
-    }
-  }
-     
-  @Override
-  protected void process(File f, DicomObject o) throws IOException,CancelException {
-    if (pm != null)
-      pm.setNote(f.getName());
 
-    sender.send(o, getTransferSyntaxUID(o));
+	Sender(final String host, final String port, final String aeTitle,
+			final TransferCapability[] tcs, final Collection<File> files,
+			final Statement statements, final ProgressMonitor pm)
+			throws IOException,ConfigurationException,InterruptedException {
+		super(statements, files);
+		final NetworkApplicationEntity localAE = new NetworkApplicationEntityBuilder()
+			.setAETitle(aeTitle)
+			.setTransferCapability(tcs)
+			.setNetworkConnection(new NetworkConnection())
+			.build();
+		final NetworkApplicationEntity remoteAE = new NetworkApplicationEntityBuilder()
+			.setNetworkConnection(new NetworkConnectionBuilder()
+				.setHostname(host)
+				.setPort(Integer.parseInt(port))
+				.build())
+			.build();
+		this.sender = new DicomSender(localAE, remoteAE);
 
-    if (pm != null) {
-      if (pm.isCanceled()) {
-        throw new CancelException();
-      }
-      pm.setProgress(++progress);
-    }
-  }
-  
-  @Override
-  void close() {
-    if (sender != null)
-      sender.close();
-    if (pm != null)
-      pm.close();
-  }
- }
+		this.pm = pm;
+	}
+
+	@Override
+	void open() throws IOException {
+		if (pm != null)
+			pm.setNote(MAKE_CONN_MSG);
+	}
+
+	
+	@Override
+	protected void process(File f, DicomObject o) throws IOException,CancelException {
+		if (pm != null)
+			pm.setNote(f.getName());
+
+		sender.send(o, getTransferSyntaxUID(o));
+
+		if (pm != null) {
+			if (pm.isCanceled()) {
+				throw new CancelException();
+			}
+			pm.setProgress(++progress);
+		}
+	}
+
+	@Override
+	void close() {
+		if (sender != null)
+			sender.close();
+		if (pm != null)
+			pm.close();
+	}
+}
