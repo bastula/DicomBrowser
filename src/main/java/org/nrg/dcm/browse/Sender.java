5b249b22f7aa26461400c565da7d328a20e7242a
Kevin Archie <karchie@maritim.us>
2008-09-23 20:00:57 +0000

    First working (?) TLS send

--- src/main/java/org/nrg/dcm/browse/Sender.java
+++ src/main/java/org/nrg/dcm/browse/Sender.java
@@ -6,8 +6,14 @@ package org.nrg.dcm.browse;
 
 import java.io.File;
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.Collection;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import javax.swing.ProgressMonitor;
 
 import org.dcm4che2.data.DicomObject;
@@ -22,42 +28,69 @@ import org.nrg.dcm.edit.Statement;
 
 final class Sender extends Exporter {
 	private static final String MAKE_CONN_MSG = "Making network connection";
+	private static final String AE_TITLE = "DicomBrowser";
+	private static final String PROTOCOL_TLS = "TLS";
 //	private static final String BAD_CONN_MSG = "Unable to make network connection: %1$s";
+
+	/**
+	 * Why do you always have to be so suspicious?
+	 */
+	private final static TrustManager[] yesManagers = new TrustManager[] {
+		new X509TrustManager() {
+			public void checkClientTrusted(X509Certificate[] chain, String authType) { /* yes, sir! */ }
+			public void checkServerTrusted(X509Certificate[] chain, String authType) { /* will do! */ }
+			public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
+		}};
+
 
 	private int progress = 0;
 	private final ProgressMonitor pm;
 	private DicomSender sender;
 
-
 	Sender(final String host, final String port, final boolean isTLS, final String aeTitle,
 			final TransferCapability[] tcs, final Collection<File> files,
 			final Statement statements, final ProgressMonitor pm) {
 		super(statements, files);
-		
-		final NetworkConnection nc;
+
+		final NetworkConnection lnc;
 		if (isTLS) {
-			nc = new NetworkConnectionBuilder()
-				.setTls(NetworkConnectionBuilder.TlsType.AES)
-				.setTlsNeedClientAuth(false)
-				.build();
+			lnc = new NetworkConnectionBuilder()
+			.setTls(NetworkConnectionBuilder.TlsType.AES)
+			.setTlsNeedClientAuth(false)
+			.build();
 		} else {
-			nc = new NetworkConnection();
+			lnc = new NetworkConnection();
 		}
 
 		final NetworkApplicationEntity localAE = new NetworkApplicationEntityBuilder()
-			.setAETitle(aeTitle)
-			.setTransferCapability(tcs)
-			.setNetworkConnection(nc)
-			.build();
-		
+		.setAETitle(AE_TITLE)
+		.setTransferCapability(tcs)
+		.setNetworkConnection(lnc)
+		.build();
+
+		final NetworkConnectionBuilder rncb = new NetworkConnectionBuilder();
+		rncb.setHostname(host).setPort(Integer.parseInt(port));
+		if (isTLS) {
+			rncb.setTls(NetworkConnectionBuilder.TlsType.AES).setTlsNeedClientAuth(false);
+		}
+
 		final NetworkApplicationEntity remoteAE = new NetworkApplicationEntityBuilder()
-			.setNetworkConnection(new NetworkConnectionBuilder()
-				.setHostname(host)
-				.setPort(Integer.parseInt(port))
-				.build())
-			.build();
-		
+		.setNetworkConnection(rncb.build())
+		.setAETitle(aeTitle)
+		.build();
+
 		this.sender = new DicomSender(localAE, remoteAE);
+		if (isTLS) {
+			try {
+				final SSLContext context = SSLContext.getInstance(PROTOCOL_TLS);
+				context.init(null, yesManagers, null);
+				this.sender.setSSLContext(context);
+			} catch (NoSuchAlgorithmException e) {
+				throw new RuntimeException(e);	// programming error
+			} catch (KeyManagementException e) {
+				throw new RuntimeException(e);
+			}
+		}
 
 		this.pm = pm;
 	}
@@ -68,7 +101,7 @@ final class Sender extends Exporter {
 			pm.setNote(MAKE_CONN_MSG);
 	}
 
-	
+
 	@Override
 	protected void process(File f, DicomObject o) throws IOException,CancelException {
 		if (pm != null)
