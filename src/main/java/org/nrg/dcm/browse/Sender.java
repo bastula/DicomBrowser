007885bb7ff820209eb5b85277ae81938eaa73b2
Kevin Archie <karchie@maritim.us>
2009-03-13 19:55:01 +0000

    DicomSender API has changed to include an Exception to be thrown if the sender doesn't like something.

--- src/main/java/org/nrg/dcm/browse/Sender.java
+++ src/main/java/org/nrg/dcm/browse/Sender.java
@@ -1,6 +1,5 @@
 /**
- * $Id: Sender.java,v 1.2 2007/04/03 04:32:23 karchie Exp $
- * Copyright (c) 2006 Washington University
+ * Copyright (c) 2006-2009 Washington University
  */
 package org.nrg.dcm.browse;
 
@@ -23,105 +22,103 @@ import org.dcm4che2.net.NetworkConnection;
 import org.dcm4che2.net.NetworkConnectionBuilder;
 import org.dcm4che2.net.TransferCapability;
 
+import org.nrg.dcm.CStoreException;
 import org.nrg.dcm.DicomSender;
 import org.nrg.dcm.edit.Statement;
 
 final class Sender extends Exporter {
-	private static final String MAKE_CONN_MSG = "Making network connection";
-	private static final String AE_TITLE = "DicomBrowser";
-	private static final String PROTOCOL_TLS = "TLS";
-
-	
-	/**
-	 * Why do you always have to be so suspicious?
-	 */
-	private final static TrustManager[] yesManagers = new TrustManager[] {
-		new X509TrustManager() {
-			public void checkClientTrusted(X509Certificate[] chain, String authType) { /* yes, sir! */ }
-			public void checkServerTrusted(X509Certificate[] chain, String authType) { /* will do! */ }
-			public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
-		}};
+  private static final String MAKE_CONN_MSG = "Making network connection";
+  private static final String AE_TITLE = "DicomBrowser";
+  private static final String PROTOCOL_TLS = "TLS";
 
 
-	private int progress = 0;
-	private final ProgressMonitor pm;
-	private DicomSender sender;
-
-	Sender(final String host, final String port, final boolean isTLS, final String aeTitle,
-			final TransferCapability[] tcs, final Collection<File> files,
-			final Statement statements, final ProgressMonitor pm) {
-		super(statements, files);
-
-		final NetworkConnection lnc;
-		if (isTLS) {
-			lnc = new NetworkConnectionBuilder()
-			.setTls(NetworkConnectionBuilder.TlsType.AES)
-			.setTlsNeedClientAuth(false)
-			.build();
-		} else {
-			lnc = new NetworkConnection();
-		}
-
-		final NetworkApplicationEntity localAE = new NetworkApplicationEntityBuilder()
-		.setAETitle(AE_TITLE)
-		.setTransferCapability(tcs)
-		.setNetworkConnection(lnc)
-		.build();
-
-		final NetworkConnectionBuilder rncb = new NetworkConnectionBuilder();
-		rncb.setHostname(host).setPort(Integer.parseInt(port));
-		if (isTLS) {
-			rncb.setTls(NetworkConnectionBuilder.TlsType.AES).setTlsNeedClientAuth(false);
-		}
-
-		final NetworkApplicationEntity remoteAE = new NetworkApplicationEntityBuilder()
-		.setNetworkConnection(rncb.build())
-		.setAETitle(aeTitle)
-		.build();
-
-		this.sender = new DicomSender(localAE, remoteAE);
-		if (isTLS) {
-			try {
-				final SSLContext context = SSLContext.getInstance(PROTOCOL_TLS);
-				context.init(null, yesManagers, null);
-				this.sender.setSSLContext(context);
-			} catch (NoSuchAlgorithmException e) {
-				throw new RuntimeException(e);	// programming error
-			} catch (KeyManagementException e) {
-				throw new RuntimeException(e);
-			}
-		}
-
-		this.pm = pm;
-	}
-
-	@Override
-	void open() throws IOException {
-		if (pm != null)
-			pm.setNote(MAKE_CONN_MSG);
-	}
+  /**
+   * Why do you always have to be so suspicious?
+   */
+  private final static TrustManager[] yesManagers = new TrustManager[] {
+    new X509TrustManager() {
+      public void checkClientTrusted(X509Certificate[] chain, String authType) { /* yes, sir! */ }
+      public void checkServerTrusted(X509Certificate[] chain, String authType) { /* will do! */ }
+      public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
+    }};
 
 
-	@Override
-	protected void process(File f, DicomObject o) throws IOException,CancelException {
-		if (pm != null)
-			pm.setNote(f.getName());
+  private int progress = 0;
+  private final ProgressMonitor pm;
+  private DicomSender sender;
 
-		sender.send(o, getTransferSyntaxUID(o));
+  Sender(final String host, final String port, final boolean isTLS, final String aeTitle,
+      final TransferCapability[] tcs, final Collection<File> files,
+      final Statement statements, final ProgressMonitor pm) {
+    super(statements, files);
 
-		if (pm != null) {
-			if (pm.isCanceled()) {
-				throw new CancelException();
-			}
-			pm.setProgress(++progress);
-		}
-	}
+    final NetworkConnection lnc;
+    if (isTLS) {
+      lnc = new NetworkConnectionBuilder()
+      .setTls(NetworkConnectionBuilder.TlsType.AES)
+      .setTlsNeedClientAuth(false)
+      .build();
+    } else {
+      lnc = new NetworkConnection();
+    }
 
-	@Override
-	void close() {
-		if (sender != null)
-			sender.close();
-		if (pm != null)
-			pm.close();
-	}
+    final NetworkApplicationEntity localAE = new NetworkApplicationEntityBuilder()
+    .setAETitle(AE_TITLE)
+    .setTransferCapability(tcs)
+    .setNetworkConnection(lnc)
+    .build();
+
+    final NetworkConnectionBuilder rncb = new NetworkConnectionBuilder();
+    rncb.setHostname(host).setPort(Integer.parseInt(port));
+    if (isTLS) {
+      rncb.setTls(NetworkConnectionBuilder.TlsType.AES).setTlsNeedClientAuth(false);
+    }
+
+    final NetworkApplicationEntity remoteAE = new NetworkApplicationEntityBuilder()
+    .setNetworkConnection(rncb.build())
+    .setAETitle(aeTitle)
+    .build();
+
+    this.sender = new DicomSender(localAE, remoteAE);
+    if (isTLS) {
+      try {
+        final SSLContext context = SSLContext.getInstance(PROTOCOL_TLS);
+        context.init(null, yesManagers, null);
+        this.sender.setSSLContext(context);
+      } catch (NoSuchAlgorithmException e) {
+        throw new RuntimeException(e);	// programming error
+      } catch (KeyManagementException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    this.pm = pm;
+  }
+
+  @Override
+  void open() throws IOException {
+    if (null != pm) pm.setNote(MAKE_CONN_MSG);
+  }
+
+
+  @Override
+  protected void process(File f, DicomObject o)
+  throws IOException,CancelException,CStoreException {
+    if (null != pm) pm.setNote(f.getName());
+
+    sender.send(o, getTransferSyntaxUID(o));
+
+    if (null != pm) {
+      if (pm.isCanceled()) {
+        throw new CancelException();
+      }
+      pm.setProgress(++progress);
+    }
+  }
+
+  @Override
+  void close() {
+    if (null != sender) sender.close();
+    if (null != pm) pm.close();
+  }
 }
