6beb9071cc9e47beaccd21ffb4ac1b7689b58d1a
Kevin A. Archie <karchie@wustl.edu>
2010-06-09 14:04:29 -0500

    Improved handling of plain file arguments. Moved StringUtils out to nrgutil

--- src/main/java/org/nrg/dcm/io/NewRootFileExporter.java
+++ src/main/java/org/nrg/dcm/io/NewRootFileExporter.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2009 Washington University
+ * Copyright (c) 2009,2010 Washington University
  */
 package org.nrg.dcm.io;
 
@@ -11,16 +11,24 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- * @author Kevin A. Archie <karchie@npg.wustl.edu>
+ * @author Kevin A. Archie <karchie@wustl.edu>
  *
  */
 public final class NewRootFileExporter extends AbstractFileExporter implements
 	DicomObjectExporter {
+    private static final char PATH_SEPARATOR_CHAR = '/';
+    private static final String PATH_SEPARATOR = "/";
+    
+    private final Logger logger = LoggerFactory.getLogger(NewRootFileExporter.class);
     private final Map<String[],File> newRoots;
 
     /**
@@ -39,9 +47,14 @@ public final class NewRootFileExporter extends AbstractFileExporter implements
 	final List<String[]> lengthSortedPaths = new ArrayList<String[]>(roots.size());
 	final Map<String[],File> pathToRoots = new HashMap<String[],File>();
 	for (final File root : roots) {
-	    final String[] path = explodePath(root.getCanonicalPath());
-	    lengthSortedPaths.add(path);
-	    pathToRoots.put(path, root);
+	    if (root.exists()) {
+		if (!root.isDirectory()) {
+		    throw new IllegalArgumentException("root " + root + " is not a directory");
+		}
+		final String[] path = explodePath(root.getCanonicalPath());
+		lengthSortedPaths.add(path);
+		pathToRoots.put(path, root);
+	    }
 	}
 	Collections.sort(lengthSortedPaths, new Comparator<String[]>() {
 	    public int compare(String[] o1, String[] o2) { return o2.length - o1.length; } // sorts in descending length order
@@ -49,9 +62,12 @@ public final class NewRootFileExporter extends AbstractFileExporter implements
 
 	// Verify that the proposed new root doesn't fall under one of the input roots.
 	final String[] explodedCanonicalRoot = explodePath(canonicalRootPath);
-	for (final String[] path : lengthSortedPaths) {
-	    if (null != relativePath(explodedCanonicalRoot, path))
-		throw new IOException("Proposed new root " + outRoot + " is under existing root " + path);
+	for (final Iterator<String[]> pathi = lengthSortedPaths.iterator(); pathi.hasNext(); ) {
+	    final String[] path = pathi.next();
+	    if (null != relativePath(explodedCanonicalRoot, path)) {
+		pathi.remove();
+		logger.warn("Proposed root {} is under existing root, skipping", path);
+	    }
 	}
 
 	// Construct the new root paths
@@ -71,7 +87,7 @@ public final class NewRootFileExporter extends AbstractFileExporter implements
 	final StringBuilder sb = new StringBuilder(super.toString());
 	sb.append(":").append(LINE_SEPARATOR);
 	for (final Map.Entry<String[],File> e : newRoots.entrySet()) {
-	    join(sb, e.getKey(), File.separator);
+	    join(sb, e.getKey(), PATH_SEPARATOR);
 	    sb.append(" -> ");
 	    sb.append(e.getValue());
 	    sb.append(LINE_SEPARATOR);
@@ -99,9 +115,8 @@ public final class NewRootFileExporter extends AbstractFileExporter implements
 	return join(new StringBuilder(), parts, separator, offset, parts.length - offset).toString();
     }
 
-    private static final String PATH_SEPARATOR_PATTERN = '\\' == File.separatorChar ? "\\\\" : File.separator;
-    private static String[] explodePath(final String path) {
-	return path.split(PATH_SEPARATOR_PATTERN);
+   private static String[] explodePath(final String path) {
+	return path.replace(File.separatorChar, PATH_SEPARATOR_CHAR).split(PATH_SEPARATOR);
     }
 
     private static String relativePath(final String[] path, final String[] root) {
@@ -113,7 +128,7 @@ public final class NewRootFileExporter extends AbstractFileExporter implements
 		return null;
 	    }
 	}
-	return join(path, File.separator, root.length);
+	return join(path, PATH_SEPARATOR, root.length);
     }
 
     private static String relativePath(final String path, final String[] root) {
