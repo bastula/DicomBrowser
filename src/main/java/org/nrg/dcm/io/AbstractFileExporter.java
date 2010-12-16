546c845879f4308d29d2a492c4fc1f9ab7ed6d4d
Kevin Archie <karchie@maritim.us>
2010-12-16 12:28:26 -0600

    fixed bug in export; version to 1.5.0b2

--- src/main/java/org/nrg/dcm/io/AbstractFileExporter.java
+++ src/main/java/org/nrg/dcm/io/AbstractFileExporter.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2009 Washington University
+ * Copyright (c) 2009,2010 Washington University
  */
 package org.nrg.dcm.io;
 
@@ -21,82 +21,97 @@ import org.nrg.dcm.edit.DicomUtils;
 import org.nrg.dcm.edit.DicomUtils.DicomObjectException;
 
 /**
- * @author Kevin A. Archie <karchie@npg.wustl.edu>
+ * @author Kevin A. Archie <karchie@wustl.edu>
  *
  */
 public abstract class AbstractFileExporter implements DicomObjectExporter {
-    private final String aeTitle;
-    
-    AbstractFileExporter(final String aeTitle) {
-	this.aeTitle = aeTitle;
-    }
-    
-    protected abstract File map(File source);
-        
-    // TODO: check against Part 10
-    private static boolean hasFileMetaInformation(final DicomObject o) {
-	return o.contains(Tag.MediaStorageSOPClassUID) && o.contains(Tag.MediaStorageSOPInstanceUID);
-    }
-    
-    /**
-     * Returns the Transfer Syntax UID for the given DicomObject
-     * @param o DicomObject
-     * @return Transfer Syntax UID
-     */
-    protected static final String getTransferSyntaxUID(final DicomObject o) {
-      final String ts = o.getString(Tag.TransferSyntaxUID);
-      // Default TS UID is Implicit VR LE (PS 3.5, Section 10)
-      return ts == null ? UID.ImplicitVRLittleEndian : ts;
-    }
-    
-    /*
-     * (non-Javadoc)
-     * @see org.nrg.dcm.io.DicomObjectExporter#close()
-     */
-    public final void close() {}
+	private final String aeTitle;
 
-    /* (non-Javadoc)
-     * @see org.nrg.dcm.io.DicomObjectExporter#export(org.dcm4che2.data.DicomObject, java.io.File)
-     */
-    public final void export(final DicomObject o, final File source)
-    throws DicomObjectException,IOException,MalformedURLException,URISyntaxException {
-	final DicomObject dcmo = null == o ? DicomUtils.read(source) : o;
-	final DicomObject fmi;
-	if (hasFileMetaInformation(dcmo)) {
-	    fmi = dcmo.fileMetaInfo();
-	} else {
-	    assert null != o;
-	    // TODO: check part 10 for the first field after file metainfo
-	    final DicomObject original = DicomUtils.read(source, Tag.ImageType);
-	    fmi = original.fileMetaInfo();
+	AbstractFileExporter(final String aeTitle) {
+		this.aeTitle = aeTitle;
 	}
-	final String tsuid = getTransferSyntaxUID(fmi);
 
-	fmi.putString(Tag.SourceApplicationEntityTitle, VR.AE, aeTitle);
+	protected abstract File map(File source);
 
-	final File dest = map(source);
-	dest.getParentFile().mkdirs();
-	final FileOutputStream fos = new FileOutputStream(dest);
-	try {
-	    final OutputStream os;
-	    if (dest.getName().endsWith(".gz")) {
-		os = new GZIPOutputStream(fos);
-	    } else {
-		os = new BufferedOutputStream(fos);
-	    }
-	    try {
-		final DicomOutputStream dos = new DicomOutputStream(os);
-		try {
-		    dos.writeFileMetaInformation(fmi);
-		    dos.writeDataset(o, tsuid);
-		} finally {
-		    dos.close();
+	private static boolean hasFileMetaInformation(final DicomObject o) {
+		return o.contains(Tag.MediaStorageSOPClassUID) && o.contains(Tag.MediaStorageSOPInstanceUID);
+	}
+
+	/**
+	 * Returns the Transfer Syntax UID for the given DicomObject
+	 * @param o DicomObject
+	 * @return Transfer Syntax UID
+	 */
+	protected static final String getTransferSyntaxUID(final DicomObject o) {
+		final String ts = o.getString(Tag.TransferSyntaxUID);
+		// Default TS UID is Implicit VR LE (PS 3.5, Section 10)
+		return ts == null ? UID.ImplicitVRLittleEndian : ts;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.nrg.dcm.io.DicomObjectExporter#close()
+	 */
+	public final void close() {}
+
+	/* (non-Javadoc)
+	 * @see org.nrg.dcm.io.DicomObjectExporter#export(org.dcm4che2.data.DicomObject, java.io.File)
+	 */
+	public final void export(final DicomObject o, final File source)
+	throws DicomObjectException,IOException,MalformedURLException,URISyntaxException {
+		final DicomObject dcmo = null == o ? DicomUtils.read(source) : o;
+		final DicomObject fmi;
+		if (hasFileMetaInformation(dcmo)) {
+			fmi = dcmo.fileMetaInfo();
+		} else {
+			assert null != o;
+			// Part 10 header ends after group 0002, but read() checks SOP Class UID
+			// to ensure file validity so we need to read at least that far.
+			final DicomObject original = DicomUtils.read(source, Tag.SOPClassUID);
+			fmi = original.fileMetaInfo();
 		}
-	    } finally {
-		os.close();
-	    }
-	} finally {
-	    fos.close();
+		final String tsuid = getTransferSyntaxUID(fmi);
+
+		fmi.putString(Tag.SourceApplicationEntityTitle, VR.AE, aeTitle);
+
+		final File dest = map(source);
+		dest.getParentFile().mkdirs();
+		IOException ioexception = null;
+		final FileOutputStream fos = new FileOutputStream(dest);
+		try {
+			final OutputStream os;
+			if (dest.getName().endsWith(".gz")) {
+				os = new GZIPOutputStream(fos);
+			} else {
+				os = new BufferedOutputStream(fos);
+			}
+			try {
+				final DicomOutputStream dos = new DicomOutputStream(os);
+				try {
+					dos.writeFileMetaInformation(fmi);
+					dos.writeDataset(o, tsuid);
+				} catch (IOException e) {
+					throw ioexception = e;
+				} finally {
+					try {
+						dos.close();
+					} catch (IOException e) {
+						throw ioexception = null == ioexception ? e : ioexception;
+					}
+				}
+			} finally {
+				try {
+					os.close();
+				} catch (IOException e) {
+					throw ioexception = null == ioexception ? e : ioexception;
+				}
+			}
+		} finally {
+			try {
+				fos.close();
+			} catch (IOException e) {
+				throw null == ioexception ? e : ioexception;
+			}
+		}
 	}
-    }
 }
