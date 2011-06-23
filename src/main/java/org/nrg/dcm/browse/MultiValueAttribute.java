efc96818055bc2b70b06acc41515fe7b42e9e7b1
Kevin A. Archie <karchie@wustl.edu>
2011-06-23 12:56:40 -0500

    added script variable dialog; fixed suffix renaming

--- src/main/java/org/nrg/dcm/browse/MultiValueAttribute.java
+++ src/main/java/org/nrg/dcm/browse/MultiValueAttribute.java
@@ -1,11 +1,9 @@
 /**
- * Copyright (c) 2006-2009 Washington University
+ * Copyright (c) 2006-2011 Washington University
  */
 package org.nrg.dcm.browse;
 
 import java.lang.Iterable;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
@@ -14,64 +12,66 @@ import org.dcm4che2.data.BasicDicomObject;
 import org.dcm4che2.data.DicomObject;
 import org.dcm4che2.util.TagUtils;
 
+import com.google.common.collect.Lists;
+
 /**
  * Represents a single DICOM attribute over multiple files, potentially
  * with multiple values.
- * @author Kevin A. Archie <karchie@npg.wustl.edu>
+ * @author Kevin A. Archie <karchie@wustl.edu>
  *
  */
 final class MultiValueAttribute implements Iterable<String> {
-  private static final DicomObject dcmo = new BasicDicomObject();       // used for attribute translation
-  private static final String EMPTY_ATTRIBUTE_VALUE = "";
-  private final int tag;
-  private final List<String> values;
-  private final boolean modified;
+    private static final DicomObject dcmo = new BasicDicomObject();       // used for attribute translation
+    private static final String EMPTY_ATTRIBUTE_VALUE = "";
+    private static final String VALUE_SEPARATOR = ", ";
+    private final int tag;
+    private final List<String> values;
+    private final boolean modified;
 
-  MultiValueAttribute(int tag, boolean isModified, String...values) {
-    this.tag = tag;
-    this.modified = isModified;
-    this.values = Collections.unmodifiableList(new ArrayList<String>(Arrays.asList(values)));
-  }
-
-  int getTag() { return tag; }
-
-  int size() { return values.size(); }
-
-  public Iterator<String> iterator() { return values.iterator(); }
-
-  String[] getValues() {
-      return values.toArray(new String[0]);
- }
-
-  String getTagString() { return TagUtils.toString(tag); }
-
-  String getNameString() { return dcmo.nameOf(tag); }
-
-  boolean isModified() { return modified; }
-  
-  /*
-   * (non-Javadoc)
-   * @see java.lang.Object#toString()
-   */
-  public String toString() {
-    final int n = values.size();
-    switch (n) {
-    case 0: return EMPTY_ATTRIBUTE_VALUE;
-    case 1: return values.get(0);
-    default:
-      StringBuilder sb = new StringBuilder();
-      sb.append(n);
-      sb.append(" values: "); // TODO: LOCALIZE THIS
-      int i = 0;
-      while (i < n-1 && i < FileSetTableModel.MAX_VALUES_SHOWN - 1) {
-        sb.append(values.get(i++)).append(", ");
-        sb.append(", ");
-      }
-      sb.append(values.get(i++));
-      if (i < n) {
-	  sb.append(FileSetTableModel.MORE_VALUES);
-      }
-      return sb.toString();
+    MultiValueAttribute(final int tag, final boolean isModified, final String...values) {
+        this.tag = tag;
+        this.modified = isModified;
+        this.values = Collections.unmodifiableList(Lists.newArrayList(values));
     }
-  }
+
+    int getTag() { return tag; }
+
+    int size() { return values.size(); }
+
+    public Iterator<String> iterator() { return values.iterator(); }
+
+    String[] getValues() {
+        return values.toArray(new String[0]);
+    }
+
+    String getTagString() { return TagUtils.toString(tag); }
+
+    String getNameString() { return dcmo.nameOf(tag); }
+
+    boolean isModified() { return modified; }
+
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    public String toString() {
+        final int n = values.size();
+        switch (n) {
+        case 0: return EMPTY_ATTRIBUTE_VALUE;
+        case 1: return values.get(0);
+        default:
+            StringBuilder sb = new StringBuilder();
+            sb.append(n);
+            sb.append(" values: "); // TODO: LOCALIZE THIS
+            int i = 0;
+            while (i < n-1 && i < FileSetTableModel.MAX_VALUES_SHOWN - 1) {
+                sb.append(values.get(i++)).append(VALUE_SEPARATOR);
+            }
+            sb.append(values.get(i++));
+            if (i < n) {
+                sb.append(FileSetTableModel.MORE_VALUES);
+            }
+            return sb.toString();
+        }
+    }
 }
---
+++ src/main/java/org/nrg/dcm/edit/VariableAssignmentManager.java
@@ -0,0 +1,156 @@
+/**
+ * Copyright (c) 2011 Washington University
+ */
+package org.nrg.dcm.edit;
+
+import java.awt.Container;
+import java.awt.GridBagConstraints;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Objects;
+import com.google.common.base.Strings;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
+
+/**
+ * @author Kevin A. Archie <karchie@wustl.edu>
+ *
+ */
+public final class VariableAssignmentManager {
+    public final static GridBagConstraints labelConstraint = new GridBagConstraints();
+    public final static GridBagConstraints valueConstraint = new GridBagConstraints();
+    static {
+        labelConstraint.gridx = 0;
+        labelConstraint.weightx = 0.2;
+
+        valueConstraint.gridx = 1;
+        valueConstraint.fill = GridBagConstraints.HORIZONTAL;
+        valueConstraint.weightx = 0.8;
+    }
+
+    private final Container container;
+    private final Map<Variable,VariableRow> variables;
+    private final Multimap<Integer,String> values;
+    private int visibleRows = 0;
+
+    public VariableAssignmentManager(final Container container,
+            final Collection<?> variables, final Multimap<Integer,String> values) {
+        this.container = container;
+        this.values = values;
+        this.variables = Maps.newLinkedHashMap();
+        for (final Object o : variables) {
+            final Variable v = (Variable)o;
+            this.variables.put(v, new VariableRow(v));
+        }
+    }
+
+    public int getVisibleRows() { return visibleRows; }
+
+    private synchronized void propagate(final Variable root) {
+        final Iterator<Map.Entry<Variable,VariableRow>> mei = variables.entrySet().iterator();
+        boolean update = false;
+        while (mei.hasNext()) {
+            final Map.Entry<Variable,VariableRow> me = mei.next();
+            if (update) {
+                me.getValue().updateDisplay();
+            } else if (root == me.getKey()) {
+                update = true;
+            }
+        }
+    }
+
+    private final class VariableRow implements DocumentListener {
+        private static final int N_READ_TRIES = 16;
+        private final Logger logger = LoggerFactory.getLogger(VariableRow.class);
+        private final Variable v;
+        private final JTextField tf;
+        private boolean isPropagating = false;
+
+        VariableRow(final Variable v) {
+            this.v = v;
+            if (v.isHidden()) {
+                tf = new JTextField();
+            } else {
+                visibleRows++;
+                final String desc = v.getDescription();
+                final JLabel label = new JLabel(Strings.isNullOrEmpty(desc) ? v.getName() : desc);
+                container.add(label, labelConstraint);
+                tf = new JTextField(getDisplayText());
+                tf.getDocument().addDocumentListener(this);
+                container.add(tf, valueConstraint);
+            }
+        }
+
+        public String getDisplayText() {
+            final String text = getText();
+            return null == text ? "" : text;
+        }
+
+        public String getText() {
+            String text = v.getValue();
+            if (null == text) {
+                final Value vi = v.getInitialValue();
+                if (null == vi) {
+                    return null;
+                } else try {
+                    return text = vi.on(values.asMap());
+                } catch (ScriptEvaluationException e) {
+                    logger.warn("unable to evaluate initial value for " + v, e);
+                    return null;
+                }
+            } else {
+                return text;
+            }
+        }
+
+        public void changedUpdate(final DocumentEvent ev) {
+            // Only set the variable value if this is a "real" text change,
+            // i.e., one that the user entered rather than a change inherited
+            // from the initial value.
+            if (!isPropagating) {
+                final Document d = ev.getDocument();
+                for (int i = 0; i < N_READ_TRIES; i++) {
+                    try {
+                        v.setValue(d.getText(0, d.getLength()));
+                        propagate(v);
+                        return;
+                    } catch (BadLocationException ignore) {}
+                }
+                logger.error("Unable to read value from {}", d);
+            }
+        }
+
+        public void insertUpdate(final DocumentEvent e) {
+            changedUpdate(e);
+        }
+
+        public void removeUpdate(final DocumentEvent e) {
+            changedUpdate(e);
+        }
+
+        public void updateDisplay() {
+            assert isPropagating == false;
+            isPropagating = true;
+
+            final String text = getDisplayText();
+            if (!Objects.equal(text, tf.getText())) {
+                tf.setText(text);
+            }
+
+            assert isPropagating == true;
+            isPropagating = false;
+        }
+    }
+}
