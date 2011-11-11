47af5b2e8944837ddecc323ba5fda75ac8b19d8f
Kevin A. Archie <karchie@wustl.edu>
2011-11-11 13:44:42 -0600

    specifying output base under an input root is an IllegalArgumentException

--- src/main/java/org/nrg/dcm/edit/CSVRemapper.java
+++ src/main/java/org/nrg/dcm/edit/CSVRemapper.java
@@ -13,15 +13,9 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.SQLException;
 import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -48,8 +42,15 @@ import org.slf4j.LoggerFactory;
 
 import au.com.bytecode.opencsv.CSVReader;
 
+import com.google.common.base.Objects;
+import com.google.common.base.Strings;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Sets;
 
 /**
  * @author Kevin A. Archie <karchie@wustl.edu>
@@ -92,7 +93,7 @@ public final class CSVRemapper {
 
         RemapContext(final String level, final Map<Integer,String> keys) {
             this.level = level;
-            this.selectionKeys = new LinkedHashMap<Integer,String>(keys);
+            this.selectionKeys = ImmutableMap.copyOf(keys);
         }
 
         public boolean equals(final Object o) {
@@ -102,10 +103,7 @@ public final class CSVRemapper {
         }
 
         public int hashCode() {
-            int result = 17;
-            result = 37*result + level.hashCode();
-            result = 37*result + selectionKeys.hashCode();
-            return result;
+            return Objects.hashCode(level, selectionKeys);
         }
 
         public String toString() {
@@ -138,10 +136,7 @@ public final class CSVRemapper {
         }
 
         public int hashCode() {
-            int result = 17;
-            result = 37 * result + context.hashCode();
-            result = 37 * result + column.hashCode();
-            return result;
+            return Objects.hashCode(context, column);
         }
 
         public String toString() {
@@ -164,12 +159,12 @@ public final class CSVRemapper {
         private static final long serialVersionUID = 1L;
         private static final String LINE_SEPARATOR = System.getProperty("line.separator");
         final Collection<RemapWithContext> underspecified;
-        final Map<RemapWithContext,Collection<String>> overspecified;
+        final Multimap<RemapWithContext,String> overspecified;
 
-        InvalidRemapsException(Collection<RemapWithContext> underspecified, Map<RemapWithContext,Collection<String>> overspecified) {
-            this.underspecified = underspecified;
+        InvalidRemapsException(Iterable<RemapWithContext> underspecified, Multimap<RemapWithContext,String> overspecified) {
+            this.underspecified = Lists.newArrayList(underspecified);
             this.overspecified = overspecified;
-            assert !underspecified.isEmpty() || !overspecified.isEmpty();
+            assert !this.underspecified.isEmpty() || !overspecified.isEmpty();           
         }
 
         public String toString() {
@@ -185,9 +180,8 @@ public final class CSVRemapper {
             if (!overspecified.isEmpty()) {
                 sb.append("The following remaps were assigned multiple values:");
                 sb.append(LINE_SEPARATOR);
-                for (final Map.Entry<RemapWithContext,Collection<String>> e : overspecified.entrySet()) {
-                    sb.append(e.getKey()).append(" : ").append(e.getValue());
-                    sb.append(LINE_SEPARATOR);
+                for (final RemapWithContext r : overspecified.keySet()) {
+                    sb.append(r).append(": ").append(overspecified.get(r));
                 }
             }
             return sb.toString();
@@ -203,11 +197,11 @@ public final class CSVRemapper {
     public CSVRemapper(final File configFile)
     throws IOException,ParseException,DocumentException,InvalidCSVException {
         final ConfigurableDirectoryRecordFactory factory = new ConfigurableDirectoryRecordFactory(configFile);
-        final List<DicomTableEntry> columns = new ArrayList<DicomTableEntry>(factory.getColumns());
+        final List<DicomTableEntry> columns = Lists.newArrayList(factory.getColumns());
 
         // Collect all remappings, and all levels for which a remapping is defined
-        final Collection<String> levels = new HashSet<String>();
-        remaps = new ArrayList<RemapColumn>();
+        final Collection<String> levels = Sets.newHashSet();
+        remaps = Lists.newArrayList();
         for (int i = 0; i < columns.size(); i++) {
             final DicomTableEntry col = columns.get(i);
             if (col.isSubstitution()) {
@@ -218,10 +212,10 @@ public final class CSVRemapper {
         }
 
         // For each level for which a remapping is defined, figure out which columns hold the selection keys.
-        selectionKeysToCols = new HashMap<String,Map<Integer,Integer>>();
+        selectionKeysToCols = Maps.newHashMap();
         for (final String level : levels) {
             final Collection<Integer> selectionTags = factory.getSelectionTags(level);
-            final Map<Integer,Integer> keysToCols = new LinkedHashMap<Integer,Integer>();
+            final Map<Integer,Integer> keysToCols = Maps.newLinkedHashMap();
             selectionKeysToCols.put(level, keysToCols);
             for (final int tag : selectionTags) {
                 boolean foundTag = false;
@@ -241,14 +235,14 @@ public final class CSVRemapper {
 
     private Map<RemapWithContext,String> makeAssignments(final CSVReader remapSpreadsheet)
     throws IOException,InvalidRemapsException {
-        final Map<RemapWithContext,Collection<String>> assignedValues = Maps.newHashMap();;
+        final Multimap<RemapWithContext,String> assignedValues = HashMultimap.create();
 
         String[] line = remapSpreadsheet.readNext();	// skip the header line
         while (null != (line = remapSpreadsheet.readNext())) {
             // Build the full remapping context for this line
-            final Map<String,RemapContext> context = new HashMap<String,RemapContext>();
+            final Map<String,RemapContext> context = Maps.newHashMap();
             for (final String level : selectionKeysToCols.keySet()) {
-                final Map<Integer,String> keyValues = new LinkedHashMap<Integer,String>();
+                final Map<Integer,String> keyValues = Maps.newLinkedHashMap();
                 for (final Map.Entry<Integer,Integer> e : selectionKeysToCols.get(level).entrySet()) {
                     keyValues.put(e.getKey(), line[e.getValue()]);
                 }
@@ -258,12 +252,9 @@ public final class CSVRemapper {
             // Now collect the assignments
             for (final RemapColumn col : remaps) {
                 final RemapWithContext remap = new RemapWithContext(context.get(col.getLevel()), col);
-                if (!assignedValues.containsKey(remap)) {
-                    assignedValues.put(remap, new LinkedHashSet<String>());
-                }
                 final String value = line[col.getIndex()];
-                if (null != value && !"".equals(value)) {
-                    assignedValues.get(remap).add(value);
+                if (!Strings.isNullOrEmpty(value)) {
+                    assignedValues.put(remap, value);
                 }
             }
         }
@@ -272,15 +263,15 @@ public final class CSVRemapper {
         // n = 0 : error: no value specified
         // n = 1 : perfect!
         // n > 1 : error: multiple values specified for same context
-        final Collection<RemapWithContext> underspecified = new LinkedList<RemapWithContext>();
-        final Map<RemapWithContext,Collection<String>> overspecified = new LinkedHashMap<RemapWithContext,Collection<String>>();
-        final Map<RemapWithContext,String> uniqueAssignments = new LinkedHashMap<RemapWithContext,String>();
-        for (final Map.Entry<RemapWithContext,Collection<String>> e : assignedValues.entrySet()) {
+        final Collection<RemapWithContext> underspecified = Lists.newArrayList();
+        final Multimap<RemapWithContext,String> overspecified = LinkedHashMultimap.create();
+        final Map<RemapWithContext,String> uniqueAssignments = Maps.newLinkedHashMap();
+        for (final Map.Entry<RemapWithContext,Collection<String>> e : assignedValues.asMap().entrySet()) {
             final Iterator<String> vi = e.getValue().iterator();
             if (vi.hasNext()) {
                 final String v = vi.next();
                 if (vi.hasNext()) {
-                    overspecified.put(e.getKey(), e.getValue());
+                    overspecified.putAll(e.getKey(), e.getValue());
                 } else {
                     uniqueAssignments.put(e.getKey(), v);
                 }
@@ -297,7 +288,7 @@ public final class CSVRemapper {
     }
 
     private static Constraint makeConstraint(final RemapWithContext remap) {
-        final Collection<ConstraintMatch> conditions = new LinkedHashSet<ConstraintMatch>();
+        final Collection<ConstraintMatch> conditions = Sets.newLinkedHashSet();
         for (Map.Entry<Integer,String> me : remap.context.selectionKeys.entrySet()) {
             conditions.add(new SimpleConstraintMatch(me.getKey(), me.getValue()));
         }
@@ -347,12 +338,12 @@ public final class CSVRemapper {
             throw new IllegalArgumentException("destination URI must be absolute");
         }
         if ("file".equals(out.getScheme())) {
-            final Set<File> roots = new LinkedHashSet<File>();	// only directories can be roots
+            final Set<File> roots = Sets.newLinkedHashSet();	// only directories can be roots
             for (final File file : files) {
                 if (file.isDirectory()) {
                     roots.add(file);
                 } else if (file.exists()) {
-                    roots.add(file.getParentFile());
+                    roots.add(file.getAbsoluteFile().getParentFile());
                 }
             }
             exporter = new NewRootFileExporter(AE_TITLE, new File(out), roots);
@@ -387,7 +378,7 @@ public final class CSVRemapper {
     public void includeStatements(final InputStream in) throws IOException {
         try {
             final ScriptApplicator applicator = new ScriptApplicator(in,
-                    new HashMap<String,ScriptFunction>(),
+                    Collections.<String,ScriptFunction>emptyMap(),
                     Collections.singletonMap("UID", uidGenerator));
             globalStatements.addAll(applicator.getStatements());
         } catch (IOException e) {
@@ -401,7 +392,7 @@ public final class CSVRemapper {
 
     private static void showUsage(final Options opts) {
         final HelpFormatter formatter = new HelpFormatter();
-        formatter.printHelp("DicomRemapper [OPTIONS] [directory | file ...]", opts);
+        formatter.printHelp("DicomRemap [OPTIONS] [directory | file ...]", opts);
     }
 
     private static URI toURI(final String path) throws URISyntaxException {
@@ -483,7 +474,7 @@ public final class CSVRemapper {
 
         final URI out = toURI(cli.getOptionValue(outputOpt.getOpt()));
 
-        final Collection<File> infiles = new ArrayList<File>();
+        final Collection<File> infiles = Lists.newArrayList();
         for (final String path : cli.getArgs()) {
             infiles.add(new File(path.toString()));
         }
